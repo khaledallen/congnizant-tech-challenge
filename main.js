97385b5698e534cd7106eb5117042b2afc9d82b3
Khaled Allen <khaled.allen@gmail.com>
2017-09-03 14:14:54 -0600

    parse the response as json

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
 (function() {
   var httpRequest;
+	var conditions = '';
+
+	var content = document.getElementById('content');
 
   makeRequest();
 
@@ -14,13 +17,18 @@
     var endpoint = 'http://localhost:3000/weather';
     httpRequest.onreadystatechange = alertContents;
     httpRequest.open('GET', endpoint, true);
+	  httpRequest.responseType = 'json';
     httpRequest.send();
   }
 
   function alertContents() {
     if (httpRequest.readyState === XMLHttpRequest.DONE) {
       if (httpRequest.status === 200) {
-        console.log('Request Successful');
+        console.log(httpRequest.response);
+	      let res = httpRequest.response;
+	      
+	      conditions = res.latitude;
+		content.innerHTML = conditions;
       } else {
         console.log('There was a problem with the request.');
       }
